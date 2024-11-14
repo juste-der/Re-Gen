6f8f1dcf4ece3d014f85756600496c02751999bd
Juste <juste.derk@gmail.com>
2024-11-14 23:04:08 +0100

    New initialized nav menu with close and open nav in script and responsive design between mobile and desktop

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,17 @@
-document.querySelectorAll(".nav-links a").forEach((link) => {
-  link.addEventListener("click", () => {
-    document.getElementById("nav_check").checked = false;
-  });
-});
+function openNav() {
+  const sidenav = document.getElementById("sidenav");
+
+  if (window.innerWidth <= 767) {
+    sidenav.style.width = "100vw";
+    sidenav.style.height = "100vh";
+  } else {
+    sidenav.style.width = "22.4rem";
+    sidenav.style.height = "48rem";
+  }
+}
+
+function closeNav() {
+  const sidenav = document.getElementById("sidenav");
+  sidenav.style.width = "0";
+  sidenav.style.height = "0";
+}
